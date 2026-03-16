8e19a183e2ae520191594614d652c42de448cb65
kiraaamel <kiraaamel@gmail.com>
2026-03-16 22:46:58 +0300

    пересоздание из-за ошибок в названии

--- jewelryproject/asgi.py
+++ jewelryproject/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for jewelryshop project.
+ASGI config for jewelryproject project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
--- jewelryproject/wsgi.py
+++ jewelryproject/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for jewelryshop project.
+WSGI config for jewelryproject project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
