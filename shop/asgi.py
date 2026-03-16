cfc04b1de4113c784979727cfa3b7c34437f2b3e
kiraaamel <kiraaamel@gmail.com>
2026-03-16 22:42:36 +0300

    errors

--- shop/asgi.py
+++ shop/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'jewelryshop.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'jewelryproject.settings')
 
 application = get_asgi_application()
--- shop/settings.py
+++ shop/settings.py
@@ -49,7 +49,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'jewelryshop.urls'
+ROOT_URLCONF = 'jewelryproject.urls'
 
 TEMPLATES = [
     {
@@ -67,7 +67,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'jewelryshop.wsgi.application'
+WSGI_APPLICATION = 'jewelryproject.wsgi.application'
 
 
 # Database
