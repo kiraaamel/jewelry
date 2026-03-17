6544afb4d593bb980cea6a6f0a1e3c1b2d21d964
kiraaamel <kiraaamel@gmail.com>
2026-03-17 20:22:10 +0300

    модель Пользователь

--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class User(AbstractUser):
+    """
+    Модель пользователя.
+    """
+    phone = models.CharField(
+        max_length=20,
+        blank=True,
+        verbose_name='Телефон'
+    )
+    bonus_points = models.IntegerField(
+        default=0,
+        verbose_name='Бонусные баллы'
+    )
+
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
+
+    def __str__(self):
+        return self.email
